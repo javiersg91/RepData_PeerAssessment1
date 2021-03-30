ace78c51ddf302db5f8f32dca7680413be97c266
javiersg91 <salasg.javier@gmail.com>
2021-03-29 20:55:18 -0600

    Just starting

---
+++ Script.R
@@ -0,0 +1,15 @@
+## 1. Code for reading in the dataset and/or processing the data
+unzip("./activity.zip")
+data <- read.csv("activity.csv")
+data$date <- as.Date(data$date, "%Y-%m-%d")
+
+## 2. Histogram of the total number of steps taken each day
+
+
+## 3. Mean and median number of steps taken each day
+## 4. Time series plot of the average number of steps taken
+## 5. The 5-minute interval that, on average, contains the maximum number of steps
+## 6. Code to describe and show a strategy for imputing missing data
+## 7. Histogram of the total number of steps taken each day after missing values are imputed
+## 8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
+## 9. All of the R code needed to reproduce the results (numbers, plots, etc.) in the report
